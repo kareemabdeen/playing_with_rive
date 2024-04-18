19cbd9add5b45643efd3781944cd82ea203c2c09
kareem abdeen <kareem.elshreef18@gmail.com>
2024-04-19 01:18:14 +0200

    Try design through device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, deprecated_member_use
 
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:playing_with_rive/on_boarding_screen.dart';
 
 void main() {
-  runApp(const PlayingWithRive());
+  runApp(
+    DevicePreview(
+      //enabled: !kReleaseMode,
+      enabled: true,
+      tools: const [
+        ...DevicePreview.defaultTools,
+        // const CustomPlugin(),
+      ],
+      builder: (context) => const PlayingWithRive(), // Wrap your app
+    ),
+  );
 }
 
 class PlayingWithRive extends StatelessWidget {
@@ -14,6 +25,9 @@ class PlayingWithRive extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
